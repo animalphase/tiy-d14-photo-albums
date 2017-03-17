4c7b6124a61304eed78079ebc5111459bf423226
charli elwonger <celwonger@gmail.com>
2017-03-17 16:11:36 -0500

    some good improvements in code and logic organization. still some to go

--- data.js
+++ data.js
@@ -3,92 +3,92 @@ var properties = ["sky", "space", "ocean", "mist", "earth"];
 var images = [
   {
     filename: 'ahmed-saffu-221040.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'alexandr-podvalny-220262.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'ander-burdain-168833.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'angel-fernandez-alonso-220762.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'arto-marttinen-133621.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'brandon-kawamura-222056.jpg',
-    album: 'Album 2',
+    album: 'Natures',
     properties: []
   },
   {
     filename: 'clem-onojeghuo-223063.jpg',
-    album: 'Album 2',
+    album: 'Natures',
     properties: []
   },
   {
     filename: 'davide-ragusa-4256.jpg',
-    album: 'Album 2',
+    album: 'Natures',
     properties: []
   },
   {
     filename: 'debashis-biswas-220439.jpg',
-    album: 'Album 2',
+    album: 'Natures',
     properties: []
   },
   {
     filename: 'denys-nevozhai-185489.jpg',
-    album: 'Album 2',
+    album: 'Natures',
     properties: []
   },
   {
     filename: 'drew-collins-207956.jpg',
-    album: 'Album 1',
+    album: 'Skylights',
     properties: []
   },
   {
     filename: 'dylan-gialanella-45725.jpg',
-    album: 'Album 3',
+    album: 'Gazing',
     properties: []
   },
   {
     filename: 'felipe-santana-330.jpg',
-    album: 'Album 3',
+    album: 'Gazing',
     properties: []
   },
   {
     filename: 'ferdinand-stohr-149422.jpg',
-    album: 'Album 3',
+    album: 'Gazing',
     properties: []
   },
   {
     filename: 'gabriel-santiago-1596.jpg',
-    album: 'Album 4',
+    album: 'Miscellany',
     properties: []
   },
   {
     filename: 'guy-bowden-95257.jpg',
-    album: 'Album 4',
+    album: 'Miscellany',
     properties: []
   },
   {
     filename: 'ihor-malytskyi-223159.jpg',
-    album: 'Album 4',
+    album: 'Miscellany',
     properties: []
   },
   {
     filename: 'ishan-seefromthesky-118581.jpg',
-    album: 'Album 4',
+    album: 'Miscellany',
     properties: []
   }
 ];
